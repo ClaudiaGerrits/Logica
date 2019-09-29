8670aa2827d67e571a8460a7363c3e9b6bdb8f39
MSI\Claudia <rtclaudiag@gmail.com>
2019-09-29 16:07:44 +0200

    Initial commit

---
+++ app/src/main/java/com/claudiagerrits/logica/LogicaActivity.kt
@@ -0,0 +1,37 @@
+package com.claudiagerrits.logica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LogicaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buttonSubmit.setOnClickListener { onSubmit ()}
+
+    }
+
+    private fun onSubmit() {
+        if (etCase1.text.toString() == getString(R.string.case_1) &&
+            etCase2.text.toString() == getString(R.string.case_2) &&
+            etCase3.text.toString() == getString(R.string.case_3) &&
+            etCase4.text.toString() == getString(R.string.case_4)
+        ) {
+            onAnswerCorrect()
+        } else {
+            onAnswerIncorrect()
+        }
+    }
+
+    private fun onAnswerCorrect() {
+        Toast.makeText(this, R.string.correct, Toast.LENGTH_LONG).show()
+    }
+
+    private fun onAnswerIncorrect() {
+        Toast.makeText(this, R.string.incorrect, Toast.LENGTH_LONG).show()
+    }
+
+}
